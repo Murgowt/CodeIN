78d7d354a700f5dd17c90689895456f537f6bff2
Gowtham Pollam <gowtham.pollam2018@vitstudent.ac.in>
2021-01-27 01:07:43 +0530

    Add files via upload

---
+++ Questions/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Tags,Questions
+import json
+# Create your views here.
+@csrf_exempt
+def CreateTag(request):
+    if(request.method=="POST"):
+        try:
+            request_data = request.body
+            decode_request_data = request.body.decode('utf-8')
+            requestDataJson = json.loads(decode_request_data)
+            tags=Tags.objects.filter(Name=requestDataJson['Tag'])
+            if(len(tags)==0):
+                tag=Tags.objects.create(Name=requestDataJson['Tag'])
+                tag.save()
+                messageData={
+                    "responseType":"success",
+                    "messageType":"success",
+                    "message":"Tag Created Successfully",
+                    "responseData":tag.Name
+                }
+                return(JsonResponse(messageData,status=200))
+            messageData={
+                "responseType":"success",
+                "messageType":"error",
+                "message":"Tag Already Exists",
+                "responseData":tags[0].Name
+            }
+            return(JsonResponse(messageData,status=200))
+        except:
+            messageData={
+                "responseType":"success",
+                "messageType":"error",
+                "message":"Inconsistent Data Provided",
+                "responseData":''
+            }
+            return(JsonResponse(messageData,status=200))
+
+
+    else:
+        #Render UI
+        pass
+
+@csrf_exempt
+def CreateQuestions(request):
+    if(request.method=="POST"):
+        try:
+            request_data=request.body
+            decode_request_data = request.body.decode('utf-8')
+            requestDataJson = json.loads(decode_request_data)
+            return(HttpResponse(requestDataJson['Text']))
+            title,difficulty,acceptance,submissions=requestDataJson['Title'],requestDataJson['Difficulty'],requestDataJson['Acceptance'],requestDataJson['Submissions']
+            likes,dislikes,text,tags=requestDataJson['Likes'],requestDataJson['DisLikes'],requestDataJson['Text'],requestDataJson['Tags']
+            question=Questions(Title=title)
+            question.Difficulty,question.Acceptance,question.Submissions=difficulty,acceptance,submissions
+            question.Likes,question.DisLikes,question.Text=likes,dislikes,text
+            question.save()
+            for i in tags:
+                tag=Tags.objects.get(Name=i)
+                question.Tags.add(tag)
+            question.save()
+            messageData={
+                    "responseType":"success",
+                    "messageType":"success",
+                    "message":"Question Created Successfully",
+                    "responseData":question.id
+                }
+            return(JsonResponse(messageData,status=200))
+        except:
+            messageData={
+                "responseType":"success",
+                "messageType":"error",
+                "message":"Inconsistent Data Provided",
+                "responseData":''
+            }
+            return(JsonResponse(messageData,status=200))
+    else:
+        #Render UI
+        pass
+
+
+
+def Problems(request):
+    
